698b9a1aea082c15aa807f086edbdec7cbb7c166
Elias Andrade <eg.souzaandrade@gmail.com>
2025-11-22 14:20:32 -0300

    Implementation of relu, leaky relu

--- activationfunction.c
+++ activationfunction.c
@@ -10,3 +10,17 @@ float tahn(float x) {
     return 2 / (1 + pow(E, -2x)) -1;
 }
 
+float relu (float x) {
+    if (x > 0) {
+        return x;
+  } else return 0;
+
+float leaky_relu(float x) {
+    if (x < 0) {
+        return x * 0.01;
+    } else return x;
+}
+
+float softmax(float x) {
+    return
+}
